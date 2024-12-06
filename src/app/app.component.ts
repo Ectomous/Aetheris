03b5097bf80c561604c7fa095f72a5b5610e8d71
Eric Mescouto <h.ericmescouto@gmail.com>
2024-12-06 16:14:11 -0300

    ✨ feat: configure application routes and providers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'Aetheris_e-commece';
-}
+export class AppComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { AddProductFormComponent } from './pages/add-product-form/add-product-form.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'add-product', component: AddProductFormComponent },
+  { path: '**', component: NotFoundComponent },
+];
