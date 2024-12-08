784ba48968c60fef66a342466ffd5dc99169142f
Eric Mescouto <h.ericmescouto@gmail.com>
2024-12-07 23:50:20 -0300

    ✨ feat: implement header component with navigation, search, and authentication controls

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CartService } from './../../services/cart.service';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  private authService: AuthService;
+  constructor(private cartService: CartService, private router: Router) {
+    this.authService = inject(AuthService);
+    this.cartService = inject(CartService);
+    this.router = inject(Router);
+  }
 
+  toggleCart() {
+    this.cartService.toggleCart();
+  }
+
+  logout() {
+    this.authService.logoutUser();
+    this.router.navigate(['login']);
+  }
+
+  userIsAutenticated() {
+    return this.authService.isAutenticated();
+  }
 }
